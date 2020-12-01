7c0a02c546c70591837b3483acb5eb5224a29550
dianacaro16 <57007447+dianacaro16@users.noreply.github.com>
2020-12-01 15:10:10 -0500

    Update models.py

--- endulzapp/models.py
+++ endulzapp/models.py
@@ -1,69 +1,78 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Administrador(models.Model):
     id_administrador = models.IntegerField(primary_key=True)
     usuario = models.CharField(max_length=50)
-
+    
+    def __str__(self):
+        return self.usuario
+    
 class Usuario_administrador(models.Model):
     usuario = models.CharField(max_length=50, primary_key=True)
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     contrasena = models.CharField(max_length=50)
     administrador = models.ForeignKey(Administrador, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nombre 
 
 class Producto(models.Model):
     id_producto = models.IntegerField(primary_key=True)
     nombre = models.CharField(max_length=50)
     costo_por_porcion = models.IntegerField()
+    
+    descripcion = models.CharField(max_length=200, null=True, blank=True)
+    
     administrador = models.ForeignKey(Administrador, on_delete=models.CASCADE)
+    
+    imagen = models.ImageField(default=None)
+    slug = models.SlugField(max_length=200, db_index=True,default=None)
+   
+    def __str__(self):
+        return self.administrador
+
 
 class Categoria(models.Model):
     nombre_producto = models.CharField(max_length=50, primary_key=True)
     nombre_categoria = models.CharField(max_length=50)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    
+    slug = models.SlugField(max_length=200,unique=True,blank=True)
+
+
 
 class Cliente(models.Model):
     id_cliente = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50,blank=True)
+    apellido = models.CharField(max_length=50,blank=True)
     correo_electronico = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=50,default=None)
 
-class Usuario_cliente(models.Model):
-    correo_electronico = models.CharField(max_length=50, primary_key=True)
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-    telefono = models.CharField(max_length=50)
-    usuario = models.CharField(max_length=50)
-    contrasena = models.CharField(max_length=50)
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.nombre 
 
 class Direccion_cliente(models.Model):
-    id_direccion = models.IntegerField(primary_key=True)
+    id_direccion = models.AutoField(primary_key=True)
+    direccion = models.CharField(max_length=50)
     barrio = models.CharField(max_length=50)
-    via_principal = models.CharField(max_length=50)
-    numero_via_principal = models.CharField(max_length=10)
-    via_auxiliar = models.CharField(max_length=50)
-    numero_via_auxiliar = models.CharField(max_length=10)
     inmueble = models.CharField(max_length=50)
     interior = models.CharField(max_length=25)
     numero = models.CharField(max_length=25)
-
-class Producto_en_carrito(models.Model):
-    id_carrito = models.IntegerField(primary_key=True)
-    unidades_por_producto = models.IntegerField()
-    cantidad_de_productos = models.IntegerField()
-    id_producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    id_cliente = models.IntegerField(default=None)
 
 class Carrito_de_compras(models.Model):
     id_carrito = models.IntegerField(primary_key=True)
     fecha_vencimiento = models.DateField(null=True, blank=True)
     fecha_compra = models.DateField(null=True, blank=True)
     fecha_entrega = models.DateField(null=True, blank=True)
-    producto_en_carrito = models.ForeignKey(Producto_en_carrito, on_delete=models.CASCADE)
+    #producto_en_carrito = models.ForeignKey(Producto_en_carrito, on_delete=models.CASCADE)
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    
+class Producto_en_carrito(models.Model):
+    unidades_por_producto = models.PositiveIntegerField()
+    id_producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    carrito = models.ForeignKey(Carrito_de_compras, on_delete=models.CASCADE)
 
 class Repartidor(models.Model):
     id_repartidor = models.IntegerField(primary_key=True)
@@ -83,6 +92,11 @@ class Factura(models.Model):
     metodo_pago = models.CharField(max_length=50)
     usuario_repartidor = models.ForeignKey(Usuario_repartidor, on_delete=models.CASCADE)
 
+class Factura_producto(models.Model):
+    id_factura_producto = models.IntegerField(primary_key=True)
+    factura = models.ForeignKey(Factura, on_delete=models.CASCADE)
+    pCarrito = models.ForeignKey(Producto_en_carrito, on_delete=models.CASCADE)
+    
 class Empleado(models.Model):
     id_empleado = models.IntegerField(primary_key=True)
     usuario = models.CharField(max_length=50)
